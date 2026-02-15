4e0f4450e8dbcca161e7104b2341f569f8ed650f
Connor Shea <connor.james.shea@gmail.com>
2026-02-15 15:20:37 -0700

    Remove ESLint and simplify code.

--- subdir/foo.tsx
+++ subdir/foo.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
+// This file will trigger the no-multi-comp rule.
+const ComponentOne = () => <div>A</div>;
+const ComponentTwo = () => <div>B</div>;
 
-// This file will trigger the no-multi-comp rule in both ESLint and Oxlint.
-const ComponentOne = () => {
-  return (
-    <div>
-      <h1>Component One</h1>
-      <p>This is the first dummy component.</p>
-    </div>
-  );
-};
-
-const ComponentTwo = () => {
-  return (
-    <div>
-      <h1>Component Two</h1>
-      <p>This is the second dummy component.</p>
-    </div>
-  );
-};
-
-// This triggers no-inferrable-type.
-// It also triggers no-unused-vars, despite that rule not being enabled in any way?
+// This triggers typescript/no-inferrable-type.
+// It also triggers typescript/no-unused-vars, despite that rule not being enabled in any way?
 const a: number = 5;
 
 export { ComponentOne, ComponentTwo };
