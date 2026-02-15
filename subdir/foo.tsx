1e6d8842b82d7e2c3306980cd989435c1a79a540
Connor Shea <connor.james.shea@gmail.com>
2026-02-15 15:34:28 -0700

    Demo the problem happening for all default rules as far back as 1.15.0.

--- subdir/foo.tsx
+++ subdir/foo.tsx
@@ -1,9 +1,15 @@
-// This file will trigger the no-multi-comp rule.
+// This file will trigger the no-multi-comp rule if it's oxlint >=1.43.0.
 const ComponentOne = () => <div>A</div>;
 const ComponentTwo = () => <div>B</div>;
 
-// This triggers typescript/no-inferrable-type.
-// It also triggers typescript/no-unused-vars, despite that rule not being enabled in any way?
+// this triggers typescript/no-inferrable-types:
 const a: number = 5;
 
 export { ComponentOne, ComponentTwo };
+
+// This triggers no-constant-binary-expression as a warning, despite that rule not being enabled!:
+if (a === {}) {
+  console.log("foo");
+}
+
+// no-constant-binary-expression is a correctness eslint rule, and so is a default rule. But with `correctness` set to off, this _should_ be disabled, right?
